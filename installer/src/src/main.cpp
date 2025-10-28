61a701c942a4d644e67847ee5ac18d27a59a174b
ThomasEscalle <60444618+ThomasEscalle@users.noreply.github.com>
2025-10-28 18:17:31 +0100

    Refactor resetResourcePath function to improve resource management and ensure proper file handling

--- installer/src/src/main.cpp
+++ installer/src/src/main.cpp
@@ -3,7 +3,9 @@
 #include <QApplication>
 
 #include "SoftwareHelpers.h"
+#include "FileHelper.h"
 #include <QDebug>
+#include <QSysInfo>
 
 
 #include <QProcess>
@@ -22,7 +24,18 @@ bool killApplication(const QString &appName) {
     return process.exitCode() == 0;
 }
 
+bool resetResourcePath() {
 
+    QString srcPath  = "D:/Pipeline_USD_2025-2026_src/resources/prism_title.png";
+    QString destPath = "//MINERVA/3d5_2526/100_RESOURCES/Pipe/Pipeline_USD_2025-2026_src/resources/prism_title.png";
+
+    if(FileHelper::FileExists(destPath)) {
+        FileHelper::DeleteFile(destPath);
+    }
+    FileHelper::CopyFile(srcPath , destPath);
+
+    return true;
+}
 
 
 
@@ -43,6 +56,13 @@ int main(int argc, char *argv[])
     qDebug()<< "Prism preferences path : " << SoftwareHelpers::getPrismPrefsPath();
 
     killApplication("Prism.exe");
+
+    /// Get the name of the computer
+    QString computerName = QSysInfo::machineHostName();
+    if(computerName == "MTP3D558") {
+        resetResourcePath();
+    }
+
 
     QApplication a(argc, argv);
     MainWizard w;
